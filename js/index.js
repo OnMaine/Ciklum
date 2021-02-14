2a9c77ba902035f1c2a0fbd0e766f270c96f6193
OnMaine <onmaine@gmail.com>
2021-02-14 02:24:04 +0200

    linter v 0.1

--- js/index.js
+++ js/index.js
@@ -7,55 +7,77 @@ let storageEvents = [];
 let isUniq = false;
 let expanded = false;
 let isEmpty;
-let filterMember ="all";
+let filterMember = 'all';
 let filteredEvents = [];
 
 function getDay() {
-  let select = document.getElementById("days");
+  const select = document.getElementById('days');
   x = select.options[select.selectedIndex].value;
-  return x;
-};
+  return +x;
+}
 
 function getTime() {
-  let select = document.getElementById("time");
+  const select = document.getElementById('time');
   y = select.options[select.selectedIndex].value;
-  return y;
-};
+  return +y;
+}
 
 function getName() {
-  let select = document.getElementById("eventName");
+  const select = document.getElementById('eventName');
   eventName = select.value;
   return eventName;
-};
+}
+
+const alertMassage = document.getElementById('alertMassage');
+const timeAlert = document.getElementById('timeAlert');
+const formAlert = document.getElementById('formAlert');
+
+function hideAlert() {
+  alertMassage.style.display = 'none';
+}
+
+function showAlert() {
+  alertMassage.style.display = 'flex';
+  setTimeout(hideAlert, 3000);
+}
+
+// checkbox Value + write placeholder func
+function getCheckedCheckBoxes() {
+  const selectedCheckBoxes = document.querySelectorAll('input.checkbox:checked');
+  const checkedValues = Array.from(selectedCheckBoxes).map((cb) => cb.value);
+  membersInvited = checkedValues;
+  const members = document.getElementById('members');
+  members.innerText = membersInvited;
+  return checkedValues;
+}
 
 // create new Event . Push to DB, serch from DB ect.
 function createEvent() {
   getCheckedCheckBoxes();
   someEvent = {
-    day: x,
-    time: y,
+    day: +x,
+    time: +y,
     members: membersInvited,
-    eventName: eventName
+    eventName,
   };
-  let storageBD = JSON.parse(localStorage.getItem('allEvents'));
+  const storageBD = JSON.parse(localStorage.getItem('allEvents'));
   if (storageBD == null) {
-  	isEmpty = true;
-  	let storageEvents = [];
+    isEmpty = true;
+    storageEvents = [];
     storageEvents.push(someEvent);
     localStorage.setItem('allEvents', JSON.stringify(storageEvents));
   } else {
-  	isEmpty = false;
+    isEmpty = false;
     storageEvents = JSON.parse(localStorage.getItem('allEvents'));
-    let check = storageEvents.some(function(elem) {
-      if (elem.day == someEvent.day && elem.time == someEvent.time) {
+    const check = storageEvents.some((elem) => {
+      if (elem.day === someEvent.day && elem.time === someEvent.time) {
         return true;
-      } else {
-        return false;
       }
+      return false;
     });
     if (check) {
-      formAlert.style.display = "none";
-      timeAlert.style.display = "block";
+      formAlert.style.display = 'none';
+      timeAlert.style.display = 'block';
       showAlert();
     } else {
       isUniq = true;
@@ -63,73 +85,60 @@ function createEvent() {
       localStorage.setItem('allEvents', JSON.stringify(storageEvents));
     }
   }
-};
+}
 
-// upload data with all events to table 
-let userTable = document.querySelector('table');
+// upload data with all events to table
+const userTable = document.querySelector('table');
 
 const updateTable = function updateTable() {
   storageEvents = JSON.parse(localStorage.getItem('allEvents'));
   if (storageEvents) {
-    for (let i = 1; i < userTable.rows.length; i++) {
-      for (let j = 1; j < userTable.rows[i].cells.length; j++)
-      storageEvents.forEach(event => {
-        if (i == event.time && j == event.day) {
-          let tableEvent = userTable.rows[i].cells[j];
-          tableEvent.id = "tableEvent";
-          let content = `<div class="events">
+    for (let i = 1; i < userTable.rows.length; i += 1) {
+      for (let j = 1; j < userTable.rows[i].cells.length; j += 1) {
+        storageEvents.forEach((event) => {
+          if (i === event.time && j === event.day) {
+            const tableEvent = userTable.rows[i].cells[j];
+            tableEvent.id = 'tableEvent';
+            const content = `<div class="events">
           <div class="eventName"> ${event.eventName} </div>
           <button id="closeBtn" type="button" class="close closeBtn" aria-label="Close">
           <span aria-hidden="true" class="close closeBtn">&times;</span>
           </button>
          </div>`;
-         tableEvent.innerHTML = content;
-        }
-      });
+            tableEvent.innerHTML = content;
+          }
+        });
+      }
     }
   }
 };
 
 window.onload = updateTable;
 
-//get select filter value
-function getMembersFilter() {
-  let select = document.getElementById("membersFilters");
-  filterMember = select.options[select.selectedIndex].value;
-  filteredEvents = [];
-  if (filterMember == "all") {
-    updateTable();
-  } else {
-    filteredTable();
-  }
-  return filterMember;
-};
-
-//update filtered table
+// update filtered table
 function filteredTable() {
   function isRightMember(elem) {
-    let eventMembers = elem.members;
-    let checked = eventMembers.some(function(member) {
-      if (member == filterMember) {
+    const eventMembers = elem.members;
+    const checked = eventMembers.some((member) => {
+      if (member === filterMember) {
         return true;
-      } else {
-        return false;
       }
+      return false;
     });
     if (checked) {
       filteredEvents.push(elem);
-    } 
+    }
   }
   storageEvents.filter(isRightMember);
-  for (let i = 1; i < userTable.rows.length; i++) {
-    for (let j = 1; j < userTable.rows[i].cells.length; j++) {
-      let tableEvent = userTable.rows[i].cells[j];
+  for (let i = 1; i < userTable.rows.length; i += 1) {
+    for (let j = 1; j < userTable.rows[i].cells.length; j += 1) {
+      const tableEvent = userTable.rows[i].cells[j];
       tableEvent.removeAttribute('id');
       userTable.rows[i].cells[j].innerHTML = '';
-      filteredEvents.forEach(event => {
-        if (i == event.time && j == event.day) {
-          tableEvent.id = "tableEvent";
-          let content = `<div class="events">
+      filteredEvents.forEach((event) => {
+        if (i === event.time && j === event.day) {
+          tableEvent.id = 'tableEvent';
+          const content = `<div class="events">
           <div class="eventName"> ${event.eventName} </div>
           <button id="closeBtn" type="button" class="close closeBtn" aria-label="Close">
           <span aria-hidden="true" class="close closeBtn">&times;</span>
@@ -140,138 +149,126 @@ function filteredTable() {
       });
     }
   }
-};
+}
 
-//checkbox multiselect
+// get select filter value
+function getMembersFilter() {
+  const select = document.getElementById('membersFilters');
+  filterMember = select.options[select.selectedIndex].value;
+  filteredEvents = [];
+  if (filterMember === 'all') {
+    updateTable();
+  } else {
+    filteredTable();
+  }
+  return filterMember;
+}
+
+// checkbox multiselect
 function showCheckboxes() {
-  let checkboxes = document.getElementById("checkboxes");
+  const checkboxes = document.getElementById('checkboxes');
   if (!expanded) {
-    checkboxes.style.display = "block";
+    checkboxes.style.display = 'block';
     expanded = true;
   } else {
-    checkboxes.style.display = "none";
+    checkboxes.style.display = 'none';
     expanded = false;
   }
-};
-
-//checkbox Value + write placeholder func
-function getCheckedCheckBoxes() {
-  let selectedCheckBoxes = document.querySelectorAll('input.checkbox:checked');
-  let checkedValues = Array.from(selectedCheckBoxes).map(cb => cb.value);
-  membersInvited = checkedValues;
-  let members = document.getElementById("members");
-  members.innerText = membersInvited;
-  return checkedValues;
-};
+}
 
 // selectAll btn func
 function toggle(source) {
-  checkboxes = document.getElementsByClassName('checkbox');
-  for (let i = 0, n = checkboxes.length; i < n; i++) {
+  const checkboxes = document.getElementsByClassName('checkbox');
+  for (let i = 0, n = checkboxes.length; i < n; i += 1) {
     checkboxes[i].checked = source.checked;
   }
-};
+}
 
-//listener for checkboxes
-let check = document.querySelectorAll('.checkbox');
-check.forEach(function(items) {
-  items.addEventListener('click', function() {
+// listener for checkboxes
+const check = document.querySelectorAll('.checkbox');
+check.forEach((items) => {
+  items.addEventListener('click', () => {
     getCheckedCheckBoxes();
   });
 });
 
 // listener for selectAll
-let btnCheckAll = document.getElementById("allChecked");
-if (window.location.pathname == "/create.html") {
-  btnCheckAll.addEventListener("click", function() {
+const btnCheckAll = document.getElementById('allChecked');
+if (window.location.pathname === '/create.html') {
+  btnCheckAll.addEventListener('click', () => {
     toggle(btnCheckAll);
     getCheckedCheckBoxes();
   });
-  console.log(window.location.pathname);
 }
 
-//validate checkboxes
-let validated = false;
+// validate checkboxes
+let inputsValid = false;
 
 function inputsValidate() {
-  let a = document.querySelector(".checkbox:checked");
+  const a = document.querySelector('.checkbox:checked');
   if (a && eventName.trim() !== '') {
     inputsValid = true;
   } else {
-    timeAlert.style.display = "none";
-    formAlert.style.display = "block";
+    timeAlert.style.display = 'none';
+    formAlert.style.display = 'block';
     showAlert();
     inputsValid = false;
   }
   return inputsValid;
-};
+}
 
-let alertMassage = document.getElementById("alertMassage");
-let timeAlert = document.getElementById("timeAlert");
-let formAlert = document.getElementById("formAlert");
-
-function hideAlert() {
-  alertMassage.style.display = "none";
-};
-
-function showAlert() {
-  alertMassage.style.display = "flex";
-  setTimeout(hideAlert, 3000);
-};
-
-
-//add Event to table on click
-if (window.location.pathname == "/create.html") {
-  let createEventBtn = document.getElementById("createEventBtn");
-  createEventBtn.addEventListener("click", function(e) {
+// add Event to table on click
+if (window.location.pathname === '/create.html') {
+  const createEventBtn = document.getElementById('createEventBtn');
+  createEventBtn.addEventListener('click', (e) => {
     e.preventDefault();
     inputsValidate();
     if (inputsValid) {
       createEvent();
       if (isUniq || isEmpty) {
-        location.href="index.html";
+        window.location.href = 'index.html';
       }
     }
   });
 }
 
-// get click coordinates 
+// get click coordinates
 let cellIndex;
 let rowIndex;
-if (window.location.pathname == "/index.html") {
-  userTable.addEventListener('click', event => { 
-    if (event.target.classList.contains('closeBtn')) { 
+
+function openModal() {
+  const overlay = document.getElementById('popup-wrapp');
+  overlay.setAttribute('style', 'visibility: visible; opacity: 1;');
+}
+
+// delete event function
+function removeEvent() {
+  function isDeleted(elem) {
+    if (rowIndex === elem.time && cellIndex === elem.day) {
+      return false;
+    }
+    return true;
+  }
+  const newStorageEvents = storageEvents.filter(isDeleted);
+  localStorage.setItem('allEvents', JSON.stringify(newStorageEvents));
+  window.location.reload();
+}
+
+if (window.location.pathname === '/index.html') {
+  userTable.addEventListener('click', (event) => {
+    if (event.target.classList.contains('closeBtn')) {
       cellIndex = event.target.closest('td').cellIndex;
       rowIndex = event.target.closest('tr').rowIndex;
       openModal();
     }
   });
-  let deleteBtnPopUp = document.getElementById("deleteBtnPopUp");
-  deleteBtnPopUp.addEventListener("click", function() {
+  const deleteBtnPopUp = document.getElementById('deleteBtnPopUp');
+  deleteBtnPopUp.addEventListener('click', () => {
     removeEvent(rowIndex, cellIndex);
   });
 }
-// delete event function
-function removeEvent(rowIndex, cellIndex) {
-  function isDeleted(elem) {
-    if (rowIndex == elem.time && cellIndex == elem.day) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  let newStorageEvents = storageEvents.filter(isDeleted);
-  localStorage.setItem('allEvents', JSON.stringify(newStorageEvents));
-  window.location.reload();
-};
-
-function openModal() {
-  let overlay = document.getElementById('popup-wrapp');
-  overlay.setAttribute("style", "visibility: visible; opacity: 1;");
-}
 
 function closeModal() {
-  let overlay = document.getElementById('popup-wrapp');
-  overlay.setAttribute("style", "visibility: hiden; opacity: 0;");
+  const overlay = document.getElementById('popup-wrapp');
+  overlay.setAttribute('style', 'visibility: hiden; opacity: 0;');
 }
-
